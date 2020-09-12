1c3858644795c5682c7be42bdad0c58110e309b6
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-09-12 08:40:43 -0300

    Relacionamento nos models

--- back-end/src/database/migrations/1599743056159-CreateAppointments.ts
+++ back-end/src/database/migrations/1599743056159-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1599576693529
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
