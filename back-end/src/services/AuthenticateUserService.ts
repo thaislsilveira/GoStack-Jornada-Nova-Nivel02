a3b86d776b8b0a0f97fbbe591de27aec8bd6fed2
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-09-16 13:43:52 -0300

    Gerando token JWT

--- back-end/src/services/AuthenticateUserService.ts
+++ back-end/src/services/AuthenticateUserService.ts
@@ -1,7 +1,7 @@
 import { getRepository } from 'typeorm';
-// import { hash } from 'bcryptjs';
-
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
 import User from '../models/User';
 
 interface Request {
@@ -11,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 class AuthenticateUserService {
   public async execute({ email, password }: Request): Promise<Response> {
@@ -28,8 +29,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'f8713da589aee6b928271c8340553230', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
