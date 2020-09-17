5dafe683670cb527b0069b98464467670cd5e0b9
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-09-17 14:11:32 -0300

    Rotas autenticadas

---
+++ back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'f8713da589aee6b928271c8340553230',
+    expiresIn: '1d',
+  },
+};
--- back-end/src/services/AuthenticateUserService.ts
+++ back-end/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -29,9 +30,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, 'f8713da589aee6b928271c8340553230', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
