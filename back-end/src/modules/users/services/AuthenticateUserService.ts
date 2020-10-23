1fd5b5d7e459ab18d03a6361f15cc1bfc64fb8bc
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-10-23 17:47:57 -0300

    usando controllers

--- back-end/src/modules/users/services/AuthenticateUserService.ts
+++ back-end/src/modules/users/services/AuthenticateUserService.ts
@@ -6,6 +6,7 @@ import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 
 import IUsersRepository from '../repositories/IUsersRepository';
+
 import User from '../infra/typeorm/entities/User';
 
 interface IRequest {
