d956041971d9739e270fa38d147affc15517bf35
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-10-24 21:01:32 -0300

    Pensando nos testes

--- back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,11 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import CreateApppointmentService from './CreateAppointmentService';
+
+describe('CreateAppoinment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
