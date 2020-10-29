93904f3d657fff317f26489d959698129756ec72
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-10-29 14:28:59 -0300

    Emails em desenvolvimento

--- back-end/src/shared/container/providers/index.ts
+++ back-end/src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMainProvider from './MailProvider/implementations/EtherealMainProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMainProvider(),
 );
