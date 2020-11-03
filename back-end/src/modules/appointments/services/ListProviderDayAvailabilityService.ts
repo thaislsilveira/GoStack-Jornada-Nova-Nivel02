f78cf4b035f345f1762968c8f33bcc3e9b8d075d
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-11-03 14:46:25 -0300

    Listando horários disponíveis

---
+++ back-end/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,60 @@
+import { injectable, inject } from 'tsyringe';
+import { getHours } from 'date-fns';
+
+import IAppoinmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderDayAvailabilityService {
+  constructor(
+    @inject('AppoinmentsRepository')
+    private appoinmentsRepository: IAppoinmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    year,
+    month,
+  }: IRequest): Promise<IResponse> {
+    const appoinments = await this.appoinmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        day,
+        year,
+        month,
+      },
+    );
+
+    const hourStart = 8;
+
+    const eachHourArray = Array.from(
+      { length: 10 },
+      (_, index) => index + hourStart,
+    );
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppoinmentInHour = appoinments.find(
+        appoinment => getHours(appoinment.date) === hour,
+      );
+      return {
+        hour,
+        available: !hasAppoinmentInHour,
+      };
+    });
+    return availability;
+  }
+}
+
+export default ListProviderDayAvailabilityService;
