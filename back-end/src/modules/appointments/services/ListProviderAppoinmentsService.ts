3c7693c1376a80b38458f5fd95c45b5a7dbabcb7
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-11-12 13:56:47 -0300

    Cache de agendamentos

--- back-end/src/modules/appointments/services/ListProviderAppoinmentsService.ts
+++ back-end/src/modules/appointments/services/ListProviderAppoinmentsService.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
-import Appoinment from '../infra/typeorm/entities/Appointment';
+import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppoinmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -15,7 +15,7 @@ interface IRequest {
 class ListProviderAppoinmentsService {
   constructor(
     @inject('AppointmentsRepository')
-    private appoinmentsRepository: IAppoinmentsRepository,
+    private appointmentsRepository: IAppoinmentsRepository,
 
     @inject('CacheProvider')
     private cacheProvider: ICacheProvider,
@@ -26,23 +26,25 @@ class ListProviderAppoinmentsService {
     day,
     year,
     month,
-  }: IRequest): Promise<Appoinment[]> {
-    const cacheData = await this.cacheProvider.recover('asd');
-
-    console.log(cacheData);
-
-    const appoinments = await this.appoinmentsRepository.findAllInDayFromProvider(
-      {
-        provider_id,
-        day,
-        year,
-        month,
-      },
+  }: IRequest): Promise<Appointment[]> {
+    const cacheKey = `provider_appoinments:${provider_id}-${year}-${month}-${day}`;
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
     );
 
-    // await this.cacheProvider.save('asd', 'asd');
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          day,
+          year,
+          month,
+        },
+      );
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
-    return appoinments;
+    return appointments;
   }
 }
 
--- back-end/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ back-end/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -25,7 +25,9 @@ export default class RedisCacheProvider implements ICacheProvider {
     // parei em 05:19
   }
 
-  public async invalidade(key: string): Promise<void> {}
+  public async invalidate(key: string): Promise<void> {
+    await this.client.del(key);
+  }
 
   public async invalidatePrefix(prefix: string): Promise<void> {
     const keys = await this.client.keys(`${prefix}:*`);
