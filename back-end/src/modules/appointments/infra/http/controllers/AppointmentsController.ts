6765dfed0ee9316d2f40918b051d9f76ade5b689
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-11-16 14:00:13 -0300

    serialização no cache

--- back-end/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ back-end/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import { classToClass } from 'class-transformer';
 
 export default class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -16,6 +17,6 @@ export default class AppointmentsController {
       user_id,
     });
 
-    return response.json(appointment);
+    return response.json(classToClass(appointment));
   }
 }
